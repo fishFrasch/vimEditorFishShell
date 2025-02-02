65e022c734a2cee2540db7156bd2a259f6310c62
fishFrasch <140188605+fishFrasch@users.noreply.github.com>
2025-02-02 21:31:21 +0500

    Create vimEditorFishShell.sh

---
+++ vimEditorFishShell.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+cat << EOF >> $HOME/.config/fish/config.fish
+#VIM EDITOR
+if [ -f $HOME/.config/fish/conf.d/vim_editor.fish ]
+       source $HOME/.config/fish/conf.d/vim_editor.fish
+end
+EOF
+
+cat << EOF >> $HOME/.config/fish/conf.d/vim_editor.fish
+set -gx EDITOR vim
+EOF
+
+exec fish
